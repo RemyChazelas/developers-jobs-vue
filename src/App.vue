e974f17bdd2080b447433526672e128738539007
RemyChazelas <remychazelas@gmail.com>
2021-10-15 18:14:32 +0200

    add button to change the name & the age

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="app">ok test pour la typographie</div>
+  <div class="app"><p>{{ name }} - {{ age }}</p></div>
+  <button @click="changeName('Zelda')">change name</button>
+  <button @click="changeAge(20)">change age</button>
 </template>
 
 <script lang="ts">
@@ -8,6 +10,21 @@ import { defineComponent } from "vue";
 export default defineComponent({
   name: "App",
   components: {},
+  data() {
+    return {
+      name: 'Link',
+      age: 25 as number | string
+    }
+  },
+  methods: {
+    changeName(name: 'string') {
+    this.name = name
+    return name
+    },
+    changeAge(age: number | string) {
+      this.age = age
+    }
+  }
 });
 </script>
 
